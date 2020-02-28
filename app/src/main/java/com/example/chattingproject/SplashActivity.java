12b1c6ef177bf757640ed786d74f671af8723897
Yoonwonwoo <dora0303@naver.com>
2020-02-28 23:50:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/chattingproject/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.chattingproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+public class SplashActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashHandler(), 3000);
+    }
+
+    private class splashHandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            finish();
+        }
+    }
+
+    //뒤로가기 못누르게 하기
+    @Override
+    public void onBackPressed() {
+    }
+}
